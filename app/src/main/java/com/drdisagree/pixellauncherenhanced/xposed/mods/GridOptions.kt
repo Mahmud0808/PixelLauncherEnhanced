a97d3f427d2bd2a8b6e690a44096f5b88b53e254
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-04-09 00:29:23 +0600

    Fix dock icons count, add app drawer columns count, and row height multiplier

--- app/src/main/java/com/drdisagree/pixellauncherenhanced/xposed/mods/GridOptions.kt
+++ app/src/main/java/com/drdisagree/pixellauncherenhanced/xposed/mods/GridOptions.kt
@@ -1,62 +1,109 @@
 package com.drdisagree.pixellauncherenhanced.xposed.mods
 
 import android.content.Context
+import com.drdisagree.pixellauncherenhanced.data.common.Constants.APP_DRAWER_GRID_COLUMNS
+import com.drdisagree.pixellauncherenhanced.data.common.Constants.APP_DRAWER_GRID_ROW_HEIGHT_MULTIPLIER
 import com.drdisagree.pixellauncherenhanced.data.common.Constants.DESKTOP_GRID_COLUMNS
 import com.drdisagree.pixellauncherenhanced.data.common.Constants.DESKTOP_GRID_ROWS
 import com.drdisagree.pixellauncherenhanced.xposed.ModPack
-import com.drdisagree.pixellauncherenhanced.xposed.mods.LauncherUtils.Companion.restartLauncher
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.XposedHook.Companion.findClass
+import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.callMethod
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.getField
+import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.hookConstructor
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.hookMethod
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.setField
 import com.drdisagree.pixellauncherenhanced.xposed.utils.XPrefs.Xprefs
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import kotlin.math.roundToInt
 
 class GridOptions (context: Context) : ModPack(context) {
 
-    private var gridRows = 4
-    private var gridColumns = 4
+    private var homeScreenGridRows = 0
+    private var homeScreenGridColumns = 0
+    private var appDrawerGridColumns = 0
+    private var appDrawerGridRowHeightMultiplier = 1f
+    private var invariantDeviceProfileInstance: Any? = null
 
     override fun updatePrefs(vararg key: String) {
         Xprefs.apply {
-            gridRows = getSliderInt(DESKTOP_GRID_ROWS, 4)
-            gridColumns = getSliderInt(DESKTOP_GRID_COLUMNS, 4)
+            homeScreenGridRows = getSliderInt(DESKTOP_GRID_ROWS, 0)
+            homeScreenGridColumns = getSliderInt(DESKTOP_GRID_COLUMNS, 0)
+            appDrawerGridColumns = getSliderInt(APP_DRAWER_GRID_COLUMNS, 0)
+            appDrawerGridRowHeightMultiplier =
+                getSliderInt(APP_DRAWER_GRID_ROW_HEIGHT_MULTIPLIER, 10) / 10f
         }
 
         when (key.firstOrNull()) {
-            DESKTOP_GRID_ROWS -> restartLauncher(mContext)
-            DESKTOP_GRID_COLUMNS -> restartLauncher(mContext)
+            DESKTOP_GRID_ROWS,
+            DESKTOP_GRID_COLUMNS,
+            APP_DRAWER_GRID_COLUMNS,
+            APP_DRAWER_GRID_ROW_HEIGHT_MULTIPLIER,
+                -> invariantDeviceProfileInstance.callMethod(
+                "onConfigChanged",
+                mContext
+            )
         }
     }
 
     override fun handleLoadPackage(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
         val invariantDeviceProfileClass = findClass("com.android.launcher3.InvariantDeviceProfile")
+        val deviceProfileClass = findClass("com.android.launcher3.DeviceProfile")
+
+        invariantDeviceProfileClass
+            .hookConstructor()
+            .runAfter { param ->
+                invariantDeviceProfileInstance = param.thisObject
+            }
+
+        deviceProfileClass
+            .hookConstructor()
+            .runAfter { param ->
+                param.thisObject.apply {
+                    if (homeScreenGridColumns != 0) {
+                        setField("numShownHotseatIcons", homeScreenGridColumns)
+                    }
+                    if (appDrawerGridColumns != 0) {
+                        setField("numShownAllAppsColumns", appDrawerGridColumns)
+                    }
+                    if (homeScreenGridColumns != 0) {
+                        setField("numShownHotseatIcons", homeScreenGridColumns)
+                    }
+                }
+            }
 
         invariantDeviceProfileClass
             .hookMethod("initGrid")
             .runAfter { param ->
-                val idp = param.thisObject
-
-                if (idp.getField("numColumns") != gridColumns || idp.getField("numRows") != gridRows) {
-                    idp.setField("numColumns", gridColumns)
-                    idp.setField("numRows", gridRows)
+                param.thisObject.apply {
+                    if (homeScreenGridRows != 0) {
+                        setField("numRows", homeScreenGridRows)
+                    }
+                    if (homeScreenGridColumns != 0) {
+                        setField("numColumns", homeScreenGridColumns)
+                        setField("numShownHotseatIcons", homeScreenGridColumns)
+                    }
                 }
             }
 
-        val deviceProfileClass = findClass("com.android.launcher3.DeviceProfile")
-
         deviceProfileClass
-            .hookMethod("updateIconSize")
-            .runBefore { param ->
-                val context = param.args[1] as Context
-                val displayMetrics = context.resources.displayMetrics
+            .hookMethod(
+                "updateIconSize",
+                "autoResizeAllAppsCells"
+            )
+            .runAfter { param ->
+                if (appDrawerGridRowHeightMultiplier == 1f) return@runAfter
 
-                val iconSizePx = (displayMetrics.widthPixels / gridColumns) * 0.8f
-                val iconSizeDp = iconSizePx / displayMetrics.density
+                val allAppsCellHeightPx = param.thisObject.getField("allAppsCellHeightPx") as Int
+                val allAppsIconDrawablePaddingPx = 0
 
-                if (param.args[0] != iconSizeDp) {
-                    param.args[0] = iconSizeDp
-                }
+                param.thisObject.setField(
+                    "allAppsCellHeightPx",
+                    (allAppsCellHeightPx * appDrawerGridRowHeightMultiplier).roundToInt()
+                )
+                param.thisObject.setField(
+                    "allAppsIconDrawablePaddingPx",
+                    allAppsIconDrawablePaddingPx
+                )
             }
     }
 }
